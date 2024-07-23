985cbdafebccaa38f7927c58d72a38c81b40ba89
ehab05 <ehabkalil5@gmail.com>
2024-07-23 09:56:42 +0300

    updated utils

--- API_test_project_part_b/infra/utils.py
+++ API_test_project_part_b/infra/utils.py
@@ -4,60 +4,68 @@ import string
 import pytz
 
 from API_test_project_part_b.infra.config_provider import ConfigProvider
+from API_test_project_part_b.infra.logger import Logger
 
 
 class Utils:
     def __init__(self):
         self._config = ConfigProvider().load_from_file("../../fake_rest_config.json")
+        self._logger = Logger("fake_rest_api.log").get_logger()
 
-    @staticmethod
-    def generate_random_number_within_range(range_values: tuple):
+    def generate_random_number_within_range(self, range_values: tuple):
         """
         this function generates a random integer within the given range (a: minimum, b: maximum)
         :param range_values: A tuple containing two integers
         :return: random integer within the given range
         """
-        if len(range_values) != 2:
-            raise ValueError("The range must be 2 elements.")
-        a, b = range_values
-        if not (isinstance(a, int) and isinstance(b, int)):
-            raise TypeError("The minimum and maximum range values must be integers")
-        return random.randint(a, b)
+        try:
+            if len(range_values) != 2:
+                raise ValueError("The range must be 2 elements.")
+            a, b = range_values
+            if not (isinstance(a, int) and isinstance(b, int)):
+                raise TypeError("The minimum and maximum range values must be integers")
+            return random.randint(a, b)
+        except Exception as e:
+            self._logger.error(f"Error generating a random number: {e}")
 
-    @staticmethod
-    def generate_random_string(string_length):
+    def generate_random_string(self, string_length):
         """
         Generates a random string of the specified length using letters.
         :param string_length:
         :return: random string
         """
-        letters = string.ascii_letters + string.digits + string.punctuation
-        return ''.join(random.choice(letters) for _ in range(string_length))
+        try:
+            letters = string.ascii_letters + string.digits + string.punctuation
+            return ''.join(random.choice(letters) for _ in range(string_length))
+        except Exception as e:
+            self._logger.error(f"Error generating a random string: {e}")
 
-    @staticmethod
-    def generate_random_iso_datetime():
+    def generate_random_iso_datetime(self):
         """
         Generates a random ISO 8601 datetime string in the format 'YYYY-MM-DDTHH:MM:SS.SSSZ'.
         :return: A random ISO 8601 datetime string.
         """
         # Generate a random date
-        start_date = datetime.datetime(2024, 7, 1, tzinfo=pytz.UTC)
-        end_date = datetime.datetime(2050, 12, 1, tzinfo=pytz.UTC)
-        delta = end_date - start_date
-        random_days = random.randrange(delta.days)
-        random_date = start_date + datetime.timedelta(days=random_days)
+        try:
+            start_date = datetime.datetime(2024, 7, 1, tzinfo=pytz.UTC)
+            end_date = datetime.datetime(2050, 12, 1, tzinfo=pytz.UTC)
+            delta = end_date - start_date
+            random_days = random.randrange(delta.days)
+            random_date = start_date + datetime.timedelta(days=random_days)
 
-        # Generate a random time
-        random_seconds = random.randint(0, 86399)
-        random_microseconds = random.randint(0, 999999)
-        random_time = datetime.timedelta(seconds=random_seconds, microseconds=random_microseconds)
+            # Generate a random time
+            random_seconds = random.randint(0, 86399)
+            random_microseconds = random.randint(0, 999999)
+            random_time = datetime.timedelta(seconds=random_seconds, microseconds=random_microseconds)
 
-        # Combine date and time
-        random_datetime = random_date + random_time
+            # Combine date and time
+            random_datetime = random_date + random_time
 
-        # Format the datetime as ISO 8601 string
-        iso_datetime_string = random_datetime.strftime('%Y-%m-%dT%H:%M:%S.') + f"{random_microseconds:06d}" + "+00:00"
-        return iso_datetime_string
+            # Format the datetime as ISO 8601 string
+            iso_datetime_string = random_datetime.strftime('%Y-%m-%dT%H:%M:%S.') + f"{random_microseconds:06d}" + "+00:00"
+            return iso_datetime_string
+        except Exception as e:
+            self._logger.error(f"Error generating a random date: {e}")
 
     @staticmethod
     def random_boolean():
