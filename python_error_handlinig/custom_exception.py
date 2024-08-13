19ae265b8048016a5de872c1e55664a5714988dd
ehab05 <ehabkalil5@gmail.com>
2024-08-13 17:07:04 +0300

    Updated project directory and modified content in REST_API_Test_Suite

--- python_error_handlinig/custom_exception.py
+++
@@ -1,5 +0,0 @@
-
-class CustomException(Exception):
-    def __init__(self, message):
-        super().__init__()
-        self._message = message
--- the_internet_herokuapp/logic/dynamic_controls_page.py
+++
@@ -1,6 +0,0 @@
-from the_internet_herokuapp.infra.base_page import BasePage
-
-
-class DynamicControlsPage(BasePage):
-    def __init__(self, driver):
-        super().__init__(driver)
